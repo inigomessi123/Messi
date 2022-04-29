0942b5d1c46f279b62f75da454e9f975e414b50f
inigomessi123 <iniko.3192@gmail.com>
2022-04-29 20:04:39 +0530

    B Work

--- src/com/base/sample.java
+++ src/com/base/sample.java
@@ -1,7 +1,21 @@
 package com.base;
 
+import java.util.Scanner;
+
 public class sample {
 	public static void main(String[] args) {
+		String original,reverse="";
+		Scanner rn=new Scanner(System.in);
+		System.out.println("Enter a string reverse");
+		original=rn.nextLine();
+		int length=original.length();
+		
+		for (int i = length-1; i>=0; i--) {
+			reverse=reverse+original.charAt(i);
+			}
+		
+		System.out.println("Reverse of entered string is:"+reverse);
+		
 		
 	}
 
